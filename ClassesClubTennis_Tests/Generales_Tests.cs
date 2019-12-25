6ab714fae7caf594228398ccb2bfa347014187a7
MartinDB <martindeboutray@hotmail.fr>
2019-12-25 22:02:06 +0100

    Création projet de tests unitaires
    contient le fichier de tests des classes générales

---
+++ ClassesClubTennis_Tests/Generales_Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassesClubTennis;
+
+namespace Generales_Tests {
+    [TestClass]
+    public class Classement_Tests {
+        [TestMethod]
+        public void ConstructeurVide_GenereClassementNC() {
+            Classement classement = new Classement();
+            Classement.ValClassement actual = classement.ValeurClassement;
+
+            Classement.ValClassement expected = Classement.ValClassement.NC;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
