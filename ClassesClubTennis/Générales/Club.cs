ea0d2dc4044b0d9e0b6c9806c01d8805ff08af2b
MartinDB <martindeboutray@hotmail.fr>
2019-12-29 18:44:48 +0100

    Creation classes Competiteur et Club

---
+++ ClassesClubTennis/Générales/Club.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassesClubTennis.Acteurs;
+
+namespace ClassesClubTennis {
+    public class Club {
+        public static Adresse AdresseClub { get; } = new Adresse("", 92500, "Rueil-Malmaison");
+
+        //public Membre CreerMembre() {
+        //    Membre membre = new Membre()
+        //    throw new NotImplementedException();
+        //}
+    }
+}
