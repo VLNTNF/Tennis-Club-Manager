78c61cc4736c69be351b65e6be78a3d8542f9a02
MartinDB <martindeboutray@hotmail.fr>
2020-01-13 16:37:52 +0100

    Edition de Competition.cs
    private class en public class
    meilleur affichage des getters et setters
    création constructeur
    enums NiveauCompetition et TypeCompetition en public enum

--- ClassesClubTennis/Evenements/Classes/Competition.cs
+++ ClassesClubTennis/Evenements/Classes/Competition.cs
@@ -5,45 +5,45 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassesClubTennis.Evenements {
-    class Competition : IEvenement {
-        public NiveauCompetition Niveau {
-            get => default;
-            set {
+    public class Competition : IEvenement {
+        public string Nom { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Duree { get; set; }
+        public TypeCompetition TypeCompetition { get; set; }
+        public NiveauCompetition Niveau { get; set; }
+        public int NbJoueursMin { get; set; }
+        public Classement ClassementMax { get; set; }
+        public List<Equipe> ListeEquipes { get; set; }
+
+        public Competition(string nom, DateTime date, TimeSpan duree, TypeCompetition typeCompetition, NiveauCompetition niveau, int nbJoueursMin, 
+            Classement classementMax, List<Equipe> listeEquipes = null) {
+            Nom = nom;
+            Date = date;
+            Duree = duree;
+            TypeCompetition = typeCompetition;
+            Niveau = niveau;
+            NbJoueursMin = nbJoueursMin;
+            ClassementMax = classementMax;
+            if (listeEquipes == null) {
+                ListeEquipes = new List<Equipe>();
+            }
+            else {
+                ListeEquipes = listeEquipes;
             }
         }
 
-        public int NbJoueursMin {
-            get => default;
-            set {
-            }
-        }
-
-        public Classement ClassementMax {
-            get => default;
-            set {
-            }
-        }
-
-        public List<Equipe> ListeEquipes {
-            get => default;
-            set {
-            }
-        }
-
-        public TypeCompetition TypeCompetition {
-            get => default;
-            set {
-            }
+        public void AjouterEquipe(Equipe nouvelleEquipe) {
+            ListeEquipes.Add(nouvelleEquipe);
         }
     }
 
-    enum NiveauCompetition {
+    public enum NiveauCompetition {
         Departemental,
         Regional,
         National
     }
 
-    enum TypeCompetition {
+    public enum TypeCompetition {
         Homme,
         Femme,
         Junior
