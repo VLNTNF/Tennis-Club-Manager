d832f797bfcc2fc458938e2ee9db9bcdd015a2ae
MartinDB <martindeboutray@hotmail.fr>
2020-01-13 16:39:05 +0100

    Edition Stage.cs
    private class en public class
    meilleur affichage des getters et setters
    création constructeur

--- ClassesClubTennis/Evenements/Classes/Stage.cs
+++ ClassesClubTennis/Evenements/Classes/Stage.cs
@@ -3,31 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassesClubTennis.Acteurs;
 
 namespace ClassesClubTennis.Evenements {
-    class Stage : IEvenement {
-        public int Age {
-            get => default;
-            set {
-            }
-        }
+    public class Stage : IEvenement {
+        public string Nom { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Duree { get; set; }
+        public int Age { get; set; }
+        public List<IEntraineur> Entraineurs { get; set; }
+        public Classement Classement { get; set; }
+        public List<Membre> ListeParticipants { get; set; }
 
-        public List<Entraineur> Entraineurs {
-            get => default;
-            set {
-            }
-        }
-
-        public Classement Classement {
-            get => default;
-            set {
-            }
-        }
-
-        public List<Membre> ListeParticipants {
-            get => default;
-            set {
-            }
+        public Stage(string nom, DateTime date, TimeSpan duree, int age, List<IEntraineur> entraineurs, Classement classement, List<Membre> listeParticipants) {
+            Nom = nom;
+            Date = date;
+            Duree = duree;
+            Age = age;
+            Entraineurs = entraineurs;
+            Classement = classement;
+            ListeParticipants = listeParticipants;
         }
     }
 }
