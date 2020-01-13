9bbbe7096b156dbab3db3c026147d6088e94262b
Valentin F <valentin.fosse.76@gmail.com>
2020-01-13 17:27:52 +0100

    Création Classe

--- ClassesClubTennis/Générales/Club.cs
+++ ClassesClubTennis/Générales/Club.cs
@@ -5,13 +5,73 @@ using System.Text;
 using System.Threading.Tasks;
 using ClassesClubTennis.Acteurs;
 
-namespace ClassesClubTennis {
-    public class Club {
-        public static Adresse AdresseClub { get; } = new Adresse("", 92500, "Rueil-Malmaison");
+namespace ClassesClubTennis
+{
+    class Club
+    {
+        private string nom;
+        private string diminutif;
+        private Adresse adresse;
+        private List<Membre> adherents;
+        private List<Membre> personnel;
 
-        //public Membre CreerMembre() {
-        //    Membre membre = new Membre()
-        //    throw new NotImplementedException();
-        //}
+        public List<Membre> Adherents
+        {
+            get { return adherents; }
+            set { adherents = value; }
+        }
+
+        public List<Membre> Personnel
+        {
+            get { return personnel; }
+            set { personnel = value; }
+        }
+
+        public string Nom
+        {
+            get { return nom; }
+        }
+
+        public string Diminutif
+        {
+            get { return diminutif; }
+        }
+
+        public Adresse Adresse
+        {
+            get { return adresse; }
+        }
+
+        public Club(string nom, string diminutif, Adresse adresse)
+        {
+            this.nom = nom;
+            this.diminutif = diminutif;
+            this.adresse = adresse;
+        }
+
+        public Club()
+        {
+            this.nom = "Tennis Club de Courbevoie";
+            this.diminutif = "TCC";
+            this.adresse = new Adresse("8 rue Gervais", 92400, "Courbevoie");
+        }
+        
+        public override string ToString()
+        {
+            return Diminutif;
+        }
+
+        public void AjouterMembre(Membre membre)
+        {
+            if(membre.Type == TypeMembre.Adherent)
+            {
+                adherents.Add(membre);
+            }
+            else
+            {
+                personnel.Add(membre);
+            }
+        }
+
     }
 }
