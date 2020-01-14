7bfbbc738aec80bd947e4e17981546c8bd671d35
Valentin F <valentin.fosse.76@gmail.com>
2020-01-14 14:38:57 +0100

    Ajout Constructeur

--- ClassesClubTennis/Acteurs/Classes/Adversaire.cs
+++ ClassesClubTennis/Acteurs/Classes/Adversaire.cs
@@ -5,5 +5,12 @@ using System.Text;
 namespace ClassesClubTennis.Acteurs {
     sealed class Adversaire : Personne, ICompetiteur {
         public Classement Classement { get; set; }
+
+        public Club Club { get; set; }
+
+        public Adversaire(string nom, string prenom, TypeSexe sexe, DateTime dateNaissance, Club club) : base(nom, prenom, sexe, dateNaissance) 
+        {
+            Club = club;
+        }
     }
 }
