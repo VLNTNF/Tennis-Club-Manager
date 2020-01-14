50d6c7a91b40c157f8db78cfcd740d37532271a3
Valentin F <valentin.fosse.76@gmail.com>
2020-01-15 00:42:44 +0100

    Modification propriété Classement

--- ClassesClubTennis/Acteurs/Interfaces/ICompetiteur.cs
+++ ClassesClubTennis/Acteurs/Interfaces/ICompetiteur.cs
@@ -4,6 +4,6 @@ using System.Text;
 
 namespace ClassesClubTennis.Acteurs {
     public interface ICompetiteur : IComparable<ICompetiteur> {
-        Classement Classement { get; }
+        Classement.ValClassement Classement { get; set; }
     }
 }
