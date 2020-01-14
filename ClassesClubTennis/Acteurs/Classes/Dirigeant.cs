69aec9bb2d49f416caae1e528c27441ef6a0c876
Valentin F <valentin.fosse.76@gmail.com>
2020-01-14 14:37:51 +0100

    Ajout Constructeur

--- ClassesClubTennis/Acteurs/Classes/Dirigeant.cs
+++ ClassesClubTennis/Acteurs/Classes/Dirigeant.cs
@@ -1,8 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace ClassesClubTennis.Acteurs {
-    abstract class Dirigeant : Membre, ISalarie {
+namespace ClassesClubTennis.Acteurs
+{
+    enum TypeDirigeant
+    { President, Adjoint, Secretaire, Tresorier }
+
+    abstract class Dirigeant : Personne, ISalarie
+    {
+        public TypeDirigeant Type { get; set; }
+        public double Salaire { get; set; }
+        public DateTime DateEntreeClub { get; set; }
+        public InfoBancaire InformationsBancaires { get; set; }
+
+        public Dirigeant(string nom, string prenom, TypeSexe sexe, DateTime dateNaissance, TypeDirigeant type, double salaire, DateTime dateEntreeClub, InfoBancaire informationsBancaires) : base(nom, prenom, sexe, dateNaissance)
+        {
+            this.Type = type;
+            this.Salaire = salaire;
+            this.DateEntreeClub = dateEntreeClub;
+            this.InformationsBancaires = informationsBancaires;
+        }
     }
 }
