460c7aa9bb63685eff0386bd6c05f076077e8664
Valentin F <valentin.fosse.76@gmail.com>
2020-01-15 00:41:07 +0100

    Ajout propriétés et constructeur

--- ClassesClubTennis/Acteurs/Classes/EntraineurSalarie.cs
+++ ClassesClubTennis/Acteurs/Classes/EntraineurSalarie.cs
@@ -2,7 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ClassesClubTennis.Acteurs {
-    sealed class EntraineurSalarie : Membre, IEntraineur, ISalarie {
+namespace ClassesClubTennis.Acteurs
+{
+    sealed class EntraineurSalarie : Membre, IEntraineur, ISalarie, ICompetiteur
+    {
+        public double Salaire { get; set; }
+        public DateTime DateEntreeClub { get; set; }
+        public InfoBancaire InformationsBancaires { get; set; }
+        public int NombreDeCoursAEffectuer { get; set; }
+
+        public EntraineurSalarie(string nom, string prenom, TypeSexe sexe, DateTime dateNaissance, string numeroTelephone, Adresse adresse, bool estCompetiteur, double salaire,
+            DateTime dateEntreeClub, InfoBancaire informationsBancaires, Classement.ValClassement classement, int nombreDeCoursAEffectuer = 0, TypeMembre type = TypeMembre.Personnel)
+            : base(nom, prenom, sexe, dateNaissance, numeroTelephone, adresse, type, estCompetiteur, classement)
+        {
+            Salaire = salaire;
+            DateEntreeClub = dateEntreeClub;
+            InformationsBancaires = informationsBancaires;
+            NombreDeCoursAEffectuer = nombreDeCoursAEffectuer;
+        }
     }
 }
