25dd84f0c197340f484b66c5f8575da0dcb22a3a
Valentin F <valentin.fosse.76@gmail.com>
2020-01-15 00:39:08 +0100

    Ajout CompareTo()

--- ClassesClubTennis/Acteurs/Classes/Adversaire.cs
+++ ClassesClubTennis/Acteurs/Classes/Adversaire.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace ClassesClubTennis.Acteurs {
-    sealed class Adversaire : Personne, ICompetiteur {
-        public Classement Classement { get; set; }
+    sealed class Adversaire : Personne, ICompetiteur 
+    {
+        public Classement.ValClassement Classement { get; set; }
 
         public Club Club { get; set; }
 
         public Adversaire(string nom, string prenom, TypeSexe sexe, DateTime dateNaissance, Club club) : base(nom, prenom, sexe, dateNaissance) 
         {
             Club = club;
+        }
+
+        public int CompareTo(ICompetiteur competiteur)
+        {
+            return Classement.CompareTo(competiteur.Classement);
         }
     }
 }
