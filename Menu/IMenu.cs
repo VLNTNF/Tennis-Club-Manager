da08b8ab481a3a67510da915e9932c6ded6433f6
MadSandman <martindeboutray@hotmail.fr>
2020-01-15 18:43:28 +0100

    Création de IMenu et MenuSimple

---
+++ Menu/IMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuNS {
+    interface IMenu : IMenuItem {
+        int SaisieMenu();
+        void AfficherMenu();
+        int SaisieNombre(int borneMin, int borneMax);
+    }
+}
