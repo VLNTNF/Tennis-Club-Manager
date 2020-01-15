96d7ea0fdc861712a59079cfa185f2cea61498a8
Valentin F <valentin.fosse.76@gmail.com>
2020-01-15 17:08:20 +0100

    Création classe

---
+++ ClassesClubTennis/Générales/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesClubTennis
+{
+    public class Utility
+    {
+        public string SaisieEntree(string entree)
+        {
+            string res;
+            do
+            {
+                Console.Write(entree);
+                res = Console.ReadLine();
+            } while ((res == "") || (res == " "));
+            return res;
+        }
+
+        public DateTime SaisieDate(string entree)
+        {
+            DateTime res;
+            string saisie;
+            bool test;
+            do
+            {
+                Console.Write(entree);
+                saisie = Console.ReadLine();
+                test = DateTime.TryParse(saisie, out res);
+            } while (test == false);
+            return res;
+        }
+    }
+}
