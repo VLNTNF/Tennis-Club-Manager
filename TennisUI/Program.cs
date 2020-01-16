a7af2c32273175a99e2deb96204593b98d0d5d04
Valentin F <valentin.fosse.76@gmail.com>
2020-01-16 19:11:29 +0100

    Mise en forme et début de tests

--- TennisUI/Program.cs
+++ TennisUI/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using MenuNS;
 using ClassesClubTennis;
 using ClassesClubTennis.Acteurs;
+using ClassesClubTennis.Evenements;
 
 namespace TennisUI
 {
@@ -13,9 +14,22 @@ namespace TennisUI
     {
         static void Main(string[] args)
         {
+            Club club = new Club();
+            //Membre membre = Membre.Creation();
+            //club.AjouterMembre(membre);
+            //Console.WriteLine();
+            //membre.Afficher();
+            club.AfficherAdherents();
+            Console.WriteLine();
+            club.Adherents[0].Afficher();
+            Console.ReadKey();
+
+            /*
+            Club club = new Club();
+
             Menu menuMembre = new Menu(
                 new List<IMenuItem>() {
-                    new Fonction(CreationMembre(), "Créer un membre"),
+                    new Fonction(CreationMembre(club), "Créer un membre"),
                     new Fonction(CreationSalarie(), "Créer un salarié"),
                     new Fonction(SupprimerMembre(), "Supprimer un membre"),
                     new Fonction(SupprimerSalarie(), "Supprimer un salarié"),
@@ -26,12 +40,14 @@ namespace TennisUI
                 }, "Module Membre", true);
 
             Menu menuCompet = new Menu(
-                new List<IMenuItem>() {
+                new List<IMenuItem>()
+                {
 
                 }, "Module Compétitions", true);
 
             Menu menuStats = new Menu(
-                new List<IMenuItem>() {
+                new List<IMenuItem>()
+                {
 
                 }, "Module Statistiques", true);
 
@@ -43,40 +59,55 @@ namespace TennisUI
                 }, "Menu général", true);
 
             menuProjet.Run();
+            */
+            
         }
-
+        
         #region Méthodes Menu Membre
-        private static FonctionDele AffichageMembre() {
+        private static FonctionDele AffichageMembre()
+        {
             throw new NotImplementedException();
         }
 
-        private static FonctionDele VerificationCotisations() {
+        private static FonctionDele VerificationCotisations()
+        {
             throw new NotImplementedException();
         }
 
-        private static FonctionDele ModifierTelephone() {
+        private static FonctionDele ModifierTelephone()
+        {
             throw new NotImplementedException();
         }
 
-        private static FonctionDele ModifierAdresse() {
+        private static FonctionDele ModifierAdresse()
+        {
             throw new NotImplementedException();
         }
 
-        private static FonctionDele SupprimerSalarie() {
+        private static FonctionDele SupprimerSalarie()
+        {
             throw new NotImplementedException();
         }
 
-        private static FonctionDele SupprimerMembre() {
+        private static FonctionDele SupprimerMembre()
+        {
             throw new NotImplementedException();
         }
 
-        private static FonctionDele CreationSalarie() {
+        private static FonctionDele CreationSalarie()
+        {
             throw new NotImplementedException();
         }
 
-        private static FonctionDele CreationMembre() {
-            Ajouter;
-        } 
+        private static FonctionDele CreationMembre(Club club)
+        {
+            /*
+            Membre membre = new Membre();
+            club.AjouterMembre(Membre.Creation());
+            */
+            throw new NotImplementedException();
+        }
         #endregion
+        
     }
 }
